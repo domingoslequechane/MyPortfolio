07e6f46198391cd7a3dad8038308bf63f8121c27
Domingos Lequchane <domingosf.lequechane@gmal.com>
2021-06-04 05:05:51 +0200

    Created contact secion, footer, configuration scroll and other settings

--- script/main.js
+++ script/main.js
@@ -5,6 +5,16 @@ $(document).ready(function(){
         } else {
             $('.navbar').removeClass("sticky");
         }
+        if(this.scroll > 500 {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').addClass(function(){
+        $('html').animate({scrollTop: 0})
     });
 
     // toggle menu/navbar script
@@ -12,6 +22,20 @@ $(document).ready(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     })
+
+    // typing animation script
+    var typed = new Typed(".typing", {
+        strings: ["Designer", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Designer", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
     // owl carousel script
     $('.carousel').ownCarousel({
